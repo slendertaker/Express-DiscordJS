53c220fe033a3161785649f1833d380a7d83f3a5
slendertaker <iqbal17711@gmail.com>
2023-12-18 09:30:50 +0700

    Updated my configuration

--- config/bot.config.ts
+++ config/bot.config.ts
@@ -14,11 +14,11 @@
 import "dotenv/config.js";
 
 const bot = {
-  name: process.env.BOT_NAME || "Sleepy4k",
-  icon: process.env.BOT_ICON || "https://i.imgur.com/8Q9ZQ2M.png",
+  name: process.env.BOT_NAME || "slendertaker",
+  icon: process.env.BOT_ICON || "https://i.scdn.co/image/ab67616d00001e02af9492f3874593a0ecb971c8",
   token: process.env.BOT_TOKEN || "",
   prefix: process.env.BOT_PREFIX || "$",
-  author: process.env.BOT_AUTHOR || "benjamin4k",
+  author: process.env.BOT_AUTHOR || "slendertaker",
   browser: process.env.BOT_BROWSER || "Discord iOS"
 }
 
